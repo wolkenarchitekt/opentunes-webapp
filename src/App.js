a9afe4cda41d88a53eaad7ede6a64d45914b73bf
Ingo Weinmann <mail@ingoweinmann.de>
2021-01-19 23:12:42 +0100

    Add bootstrap and initial tracks API list request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+function TrackList() {
+  const [data, setData] = useState({ results: [] });
+ 
+  useEffect(async () => {
+    const result = await axios(
+      'http://localhost:8000/track/api/tracks/',
+    );
+    setData(result.data)
+  }, []);
+
   return (
-    <p1>Hello</p1>
+    <div>
+    {data.results.map(item => (
+      <div>
+        <p>{item.artist}</p>
+        <p>{item.title}</p>
+      </div>
+    ))}
+    </div>
   );
+
 }
 
+
+function App() {
+ 
+  return (
+    <TrackList></TrackList>
+  );
+}
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
