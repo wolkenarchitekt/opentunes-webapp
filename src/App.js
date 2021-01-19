4079e2d9c0631edb30555649ed0322f7623575a4
Ingo Weinmann <mail@ingoweinmann.de>
2021-01-19 23:20:03 +0100

    Format with prettier

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function TrackList() {
   const [data, setData] = useState({ results: [] });
- 
+
   useEffect(async () => {
-    const result = await axios(
-      'http://localhost:8000/track/api/tracks/',
-    );
-    setData(result.data)
+    const result = await axios("http://localhost:8000/track/api/tracks/");
+    setData(result.data);
   }, []);
 
   return (
     <div>
-    {data.results.map(item => (
-      <div>
-        <p>{item.artist}</p>
-        <p>{item.title}</p>
-      </div>
-    ))}
+      {data.results.map((item) => (
+        <div>
+          <p>{item.artist}</p>
+          <p>{item.title}</p>
+        </div>
+      ))}
     </div>
   );
-
 }
-
 
 function App() {
- 
-  return (
-    <TrackList></TrackList>
-  );
+  return <TrackList></TrackList>;
 }
- 
+
 export default App;
